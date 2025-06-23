255c0bb356f78ae398368efe19b783e5c732794c
DakshGoswami5 <dakshgoswami2@gmail.com>
2025-06-23 12:50:32 +0530

    Start Recipe project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
 
-import Nav from "./components/Nav";
-import Mainroutes from "./routes/Mainroutes";
-
 const App = () => {
   return (
-    
-    <div className="w-screen h-screen bg-gray-800 text-white px-[10%]">
-      <Nav></Nav>
-      <Mainroutes></Mainroutes>
-  
-    </div>
+    <div>App</div>
   )
 }
 
